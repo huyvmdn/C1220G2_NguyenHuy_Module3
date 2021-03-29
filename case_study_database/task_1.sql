ddbdeb40bcfa198dc2b06f299c3cd7d67446b836
huyvmdn <huyvm.dn@gmail.com>
2021-03-29 08:16:39 +0700

    update case study

---
+++ case_study_database/task_1.sql
@@ -0,0 +1,48 @@
+use quan_ly_furama;
+
+
+
+insert into trinhDo(ten_trinhDo)
+values
+("Trung cấp"),
+("Cao đẳng"),
+("Đại học"),
+("sau đại học");
+
+insert into viTri(ten_viTri)
+values
+("giám đốc"),
+("quản lý"),
+("giám sát"),
+("chuyên viên"),
+("phục vụ"),
+("Lễ tân");
+
+
+insert into LoaiDichVu(ten_LoaiDichVu)
+values
+("Villa"),
+("House"),
+("Room");
+
+insert into loaiKhach(ten_loaiKhach)
+values
+("Diamond"),
+("Platinium"),
+("Gold"),
+("Silver"),
+("Member");
+
+insert into dichvudikem(ten_dichVuDiKem,gia,donvi,trangThaiKhaDung)
+values
+("massage",0,1,"available"),
+("karaoke",0,1,"available"),
+("thức ăn,",0,1,"not available"),
+("nước uống",0,1,"available"),
+("thuê xe di chuyển tham quan resort",0,1,"not available");
+
+
+
+
+
+
